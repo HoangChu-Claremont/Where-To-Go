b02b0bdde1600626300918428ae44da5cb2945bf
Hoang Chu <hoangchu223@fb.com>
2022-06-21 16:04:12 -0700

    Create login and homepage layout

--- app/src/main/java/com/example/where_to_go/MainActivity.java
+++ app/src/main/java/com/example/where_to_go/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.where_to_go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity{
+
+    private static final String TAG = "MainActivity";
+    Intent i; // Navigate to either login or home screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +20,21 @@ public class MainActivity extends AppCompatActivity{
         
         if (ParseUser.getCurrentUser() == null) {
             // If someone has logged out already
+            Log.i(TAG, "goLogin");
             goLoginActivity();
         } else {
+            Log.i(TAG, "goHome");
             goHomeActivity();
         }
     }
 
     private void goHomeActivity() {
+        i = new Intent(this, HomeActivity.class);
+        startActivity(i);
     }
 
     private void goLoginActivity() {
+        i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
