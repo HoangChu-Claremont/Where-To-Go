2b73d2e941cec578976df6890bb463c6fca8fa68
Hoang Chu <hoangchu223@fb.com>
2022-06-21 13:58:35 -0700

    Connect Database to App by ParseSDK

--- app/src/main/java/com/example/where_to_go/MainActivity.java
+++ app/src/main/java/com/example/where_to_go/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.where_to_go;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.parse.ParseUser;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if (ParseUser.getCurrentUser() == null) {
+            // If someone has logged out already
+            goLoginActivity();
+        } else {
+            goHomeActivity();
+        }
+    }
+
+    private void goHomeActivity() {
+    }
+
+    private void goLoginActivity() {
     }
 }
