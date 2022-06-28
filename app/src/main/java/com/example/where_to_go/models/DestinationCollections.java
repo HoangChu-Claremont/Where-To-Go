456431e117caa9e61ae05310678e985a2f2aa71a
Hoang Chu <hoangchu223@fb.com>
2022-06-27 18:31:56 -0700

    Renamed bundlePathName and bundlePathImageUrl to collectionName and collectionImageUrl

--- app/src/main/java/com/example/where_to_go/models/DestinationCollections.java
+++ app/src/main/java/com/example/where_to_go/models/DestinationCollections.java
@@ -2,20 +2,20 @@ package com.example.where_to_go.models;
 
 public class DestinationCollections {
 
-    private String bundlePathName;
-    private String bundlePathImageUrl;
+    private String collectionName;
+    private String collectionImageUrl;
 
-    public DestinationCollections(String _bundlePathName, String _bundlePathImageUrl) {
-        bundlePathName = _bundlePathName;
-        bundlePathImageUrl = _bundlePathImageUrl;
+    public DestinationCollections(String _collectionName, String _collectionImageUrl) {
+        collectionName = _collectionName;
+        collectionImageUrl = _collectionImageUrl;
     }
 
     // GETTER
     public String getPathName() {
-        return bundlePathName;
+        return collectionName;
     }
 
     public String getPathImageUrl() {
-        return bundlePathImageUrl;
+        return collectionImageUrl;
     }
 }
