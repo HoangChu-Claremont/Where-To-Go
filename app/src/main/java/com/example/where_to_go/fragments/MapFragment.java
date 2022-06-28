6d123a41c05f6d59c8dbe4513da3de1f6fa0dbd2
Hoang Chu <hoangchu223@fb.com>
2022-06-28 15:17:33 -0700

    Create Google Map View for all listed destinations

--- app/src/main/java/com/example/where_to_go/fragments/MapFragment.java
+++ app/src/main/java/com/example/where_to_go/fragments/MapFragment.java
@@ -1,6 +1,7 @@
 package com.example.where_to_go.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,13 +16,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.where_to_go.MapActivity;
 import com.example.where_to_go.R;
 import com.example.where_to_go.adapters.FilteredDestinationAdapter;
 import com.example.where_to_go.models.Destination;
 import com.example.where_to_go.utilities.FilterAlgorithm;
 import com.example.where_to_go.utilities.YelpClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
@@ -31,6 +38,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import okhttp3.Call;
@@ -41,6 +49,7 @@ public class MapFragment extends Fragment {
     private static final String TAG = "MapFragment";
     private FilteredDestinationAdapter filteredDestinationAdapter;
     private List<Destination> filteredDestinations;
+//    private GoogleMap googleMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,39 +73,34 @@ public class MapFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Featured Destination
+        setFilteredDestinationRecyclerView();
+
         // Get a handle to the fragment and register the callback.
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
         // When Google Map is loaded, test that we captured the fragment
         assert supportMapFragment != null;
-        supportMapFragment.getMapAsync(googleMap -> {
+        supportMapFragment.getMapAsync( googleMap -> {
+            getFilteredDestination(googleMap);
             // When clicking on map
-            googleMap.setOnMapClickListener(latLng -> {
-                MarkerOptions markerOptions = new MarkerOptions();
-                // Set position on Marker
-                markerOptions.position(latLng);
-                // Set title of the marker
-                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-                // Remove all marker
-                googleMap.clear();
-                // Animating to Zoom the marker
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                // Add marker on Map
-                googleMap.addMarker(markerOptions);
-            });
+//            LatLng sydney = new LatLng(-33.852, 151.211);
+//            googleMap.addMarker(new MarkerOptions()
+//                    .position(sydney)
+//                    .title("Marker in Sydney"));
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         });
+
         Log.i(TAG, "Map Created");
 
-        // Featured Destination
-        getFilteredDestination();
-        setFilteredDestinationRecyclerView();
+
+//        setGoogleMap();
     }
+
 
     // HELPER METHODS
 
-    private void getFilteredDestination() {
+    private void getFilteredDestination(GoogleMap googleMap) {
 
-        // query for top 10 paths based on average
-        filteredDestinations = new ArrayList<>();
         final YelpClient topPath = new YelpClient();
 
         topPath.getResponse(-122.1483654685629, 37.484668049999996, 10, new Callback() {
@@ -115,6 +119,21 @@ public class MapFragment extends Fragment {
                     ((Activity) requireContext()).runOnUiThread(() -> {
                         //change View Data
                         filteredDestinationAdapter.notifyDataSetChanged();
+
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+                        for (Destination destination : filteredDestinations) {
+                            LatLng coordinate = new LatLng(destination.getLatitude(), destination.getLongitude());
+                            MarkerOptions marker = new MarkerOptions();
+                            googleMap.addMarker(marker.position(coordinate).title(destination.getTitle()));
+                            builder.include(marker.getPosition());
+                        }
+
+                        LatLngBounds bounds = builder.build();
+                        // TODO: Calculate Padding
+                        int padding = 420; // More values = More zooming out
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                        googleMap.moveCamera(cu);
                     });
 
                 } catch (IOException | JSONException e) {
@@ -130,6 +149,9 @@ public class MapFragment extends Fragment {
     }
 
     private void setFilteredDestinationRecyclerView() {
+        // query for top 10 paths based on average
+        filteredDestinations = new ArrayList<>();
+
         RecyclerView rvDestinations = requireView().findViewById(R.id.rvDestinations);
         // Create the Adapter
         filteredDestinationAdapter = new FilteredDestinationAdapter(getContext(), filteredDestinations);
@@ -141,5 +163,10 @@ public class MapFragment extends Fragment {
 
         // Set the Adapter on RecyclerView
         rvDestinations.setAdapter(filteredDestinationAdapter);
+    }
+
+    private void setGoogleMap() {
+        startActivity(new Intent(getContext(), MapActivity.class));
+        requireActivity().finish();
     }
 }
