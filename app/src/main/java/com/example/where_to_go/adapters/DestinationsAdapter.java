8fb0b355dced3b1cf43b04b409b7adadf4ac27ba
Hoang Chu <hoangchu223@fb.com>
2022-07-12 15:21:49 -0700

    Added logs to Destination Adapter

--- app/src/main/java/com/example/where_to_go/adapters/DestinationsAdapter.java
+++ app/src/main/java/com/example/where_to_go/adapters/DestinationsAdapter.java
@@ -8,24 +8,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.example.where_to_go.DestinationDetailsActivity;
-import com.example.where_to_go.MainActivity;
 import com.example.where_to_go.R;
 import com.example.where_to_go.models.Destination;
-
+import org.jetbrains.annotations.Contract;
 import java.util.Collections;
 import java.util.List;
 
 public class DestinationsAdapter extends RecyclerView.Adapter<DestinationsAdapter.FilteredDestinationViewHolder> {
     private static final String TAG = "DestinationsAdapter";
+
     public Context context;
     private final List<Destination> destinations;
 
@@ -49,10 +47,13 @@ public class DestinationsAdapter extends RecyclerView.Adapter<DestinationsAdapte
 
     @Override
     public int getItemCount() {
+        Log.i(TAG, "getItemCount");
         return destinations.size();
     }
 
     public void onItemMove(int fromPosition, int toPosition) {
+        Log.i(TAG, "onItemMove");
+
         if (fromPosition < toPosition) {
             for (int leftWard = fromPosition; leftWard < toPosition; ++leftWard) {
                 Collections.swap(destinations, leftWard, leftWard + 1);
@@ -69,6 +70,7 @@ public class DestinationsAdapter extends RecyclerView.Adapter<DestinationsAdapte
 
     public class FilteredDestinationViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private static final String TAG = "FeaturedTourViewHolder";
+
         private final ImageView ivDestinationImage;
         private final TextView tvDestinationName;
 
@@ -91,16 +93,14 @@ public class DestinationsAdapter extends RecyclerView.Adapter<DestinationsAdapte
         public void onClick(View v) {
             // gets item position
             int position = getAdapterPosition();
+
             // make sure the position is valid, i.e. actually exists in the view
             Log.i(TAG, String.valueOf(position));
             if (position != RecyclerView.NO_POSITION) {
-                // get the post at the position, this won't work if the class is static
                 Destination destination = destinations.get(position);
-                // create intent for the new activity
                 Intent intent = new Intent(context, DestinationDetailsActivity.class);
 
                 addInformationToIntent(intent, destination);
-
                 goToDestinationDetails(intent);
             }
         }
@@ -109,20 +109,22 @@ public class DestinationsAdapter extends RecyclerView.Adapter<DestinationsAdapte
 
         private void goToDestinationDetails(Intent intent) {
             FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
+
             FragmentTransaction transaction = fragmentManager.beginTransaction();
             transaction.addToBackStack(null);
             transaction.commit();
+
             context.startActivity(intent);
         }
 
+        @NonNull
+        @Contract("_, _ -> param1")
         private Intent addInformationToIntent(@NonNull Intent _intent, @NonNull Destination destination) {
             // Add information to the intent
             _intent.putExtra("destination_photo", destination.getImageUrl());
             _intent.putExtra("destination_name", destination.getLocationName());
             _intent.putExtra("destination_phone", destination.getPhone());
             _intent.putExtra("destination_rating", destination.getRating());
-
-            // Add distance
             _intent.putExtra("destination_distance", destination.getDistance());
             _intent.putExtra("destination_address", destination.getAddress());
 
